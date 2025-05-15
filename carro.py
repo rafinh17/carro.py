d228da5edd43b412fdf610e13b4ff98caf03bac4
rafinh17 <fafa.macisilva15@gmail.com>
2025-05-15 16:42:48 -0300

    Add files via upload

---
+++ carro.py
@@ -0,0 +1,12 @@
+#pedindo digitar o nome de carro
+nome-carro = input("digite o nome do carro: ")
+#pedindo ovalor do carro 
+valor_carro = float(input("digite o valor do carro:"))
+#pedindo o consumo de combustivel 
+consumo_de_combustivel = float(input('digite a quantidade de combustivel que o veiculo consome:'))
+
+#organizando a impressao no terminal 
+print('------------------------')
+print('o modelo do carro é :', carro)
+print(f'o valor do carro é {valor_carro}')
+print(f'o consumo de combustivel é {consumo_de_combustivel}')
